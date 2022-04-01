07bba6b72c847d6313e0bbac66b02bd20dfad3b5
Aimless397 <douglaszs397@hotmail.com>
2022-04-01 18:38:40 -0500

    chore: updated app.module for GraphQL compatibility

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,9 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
 import { DisksModule } from './disks/disks.module';
 import { OrdersModule } from './orders/orders.module';
@@ -7,8 +11,23 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    EventEmitterModule.forRoot(),
     ConfigModule.forRoot({
       envFilePath: '.env',
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema/schema.gql'),
+      sortSchema: true,
+      playground: true,
+      formatError: (error) => {
+        const graphQLFormattedError = {
+          name: error.name,
+          message: error.message,
+        };
+        return graphQLFormattedError;
+      },
     }),
     AuthModule,
     DisksModule,
