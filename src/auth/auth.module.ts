be031dda26a5728f28fc8130a75d06b136809ae2
Aimless397 <douglaszs397@hotmail.com>
2022-04-01 18:20:11 -0500

    chore: updated auth module sendgrid service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { UsersModule } from 'src/users/users.module';
+import { UsersModule } from '../users/users.module';
 import { AuthController } from './controllers/auth.controller';
-import { JwtStrategy } from './jwt.strategy';
-import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
 import { AuthService } from './services/auth.service';
+import { SendgridService } from '../email/services/sendgrid.service';
 
 @Module({
   imports: [
@@ -19,7 +20,7 @@ import { AuthService } from './services/auth.service';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy, SendgridService],
   exports: [AuthService],
 })
 export class AuthModule {}
