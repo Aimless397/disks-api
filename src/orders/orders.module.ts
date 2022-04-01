e02afd356005f5c96b25db3b5aa1cd45ce8ded1e
Aimless397 <douglaszs397@hotmail.com>
2022-04-01 18:32:48 -0500

    chore: updated order module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
-import { DisksService } from '../disks/services/disks.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DisksModule } from '../disks/disks.module';
+import { DisksService } from '../disks/services/disks.service';
+import { SendgridService } from '../email/services/sendgrid.service';
+import { FilesService } from '../files/files.service';
+import { UsersService } from '../users/services/users.service';
+import { UsersModule } from '../users/users.module';
 import { OrdersController } from './controllers/orders.controller';
+import { OrderUpdatedListener } from './listeners/order-updated.listener';
+import { OrdersResolver } from './resolvers/orders.resolver';
 import { OrdersService } from './services/orders.service';
 
 @Module({
-  imports: [DisksModule],
+  imports: [UsersModule, DisksModule, ConfigModule],
   controllers: [OrdersController],
-  providers: [OrdersService, DisksService],
+  providers: [
+    OrdersService,
+    DisksService,
+    FilesService,
+    OrdersResolver,
+    SendgridService,
+    OrderUpdatedListener,
+    UsersService,
+  ],
 })
 export class OrdersModule {}
