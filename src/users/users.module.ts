73e1fdcfb31dd648b1dca1c019fa9b46909ea408
Aimless397 <douglaszs397@hotmail.com>
2022-04-01 18:37:37 -0500

    chore: updated user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './controllers/users.controller';
+import { UsersResolver } from './resolvers/users.resolver';
 import { UsersService } from './services/users.service';
 
 @Module({
-  providers: [UsersService],
-  exports: [UsersService],
   controllers: [UsersController],
+  providers: [UsersService, UsersResolver],
+  exports: [UsersService],
 })
 export class UsersModule {}
