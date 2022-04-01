83d4718911c68171127b0abc0850234f6c25f075
Aimless397 <douglaszs397@hotmail.com>
2022-04-01 18:26:43 -0500

    chore: updated disk module

--- src/disks/disks.module.ts
+++ src/disks/disks.module.ts
@@ -3,10 +3,14 @@ import { AuthModule } from '../auth/auth.module';
 import { UsersService } from '../users/services/users.service';
 import { DisksController } from './controllers/disks.controller';
 import { DisksService } from './services/disks.service';
+import { FilesService } from '../files/files.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DisksResolver } from './resolvers/disks.resolver';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, ConfigModule],
   controllers: [DisksController],
-  providers: [DisksService, UsersService],
+  providers: [DisksService, UsersService, FilesService, DisksResolver],
+  exports: [DisksService],
 })
 export class DisksModule {}
