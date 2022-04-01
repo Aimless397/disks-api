92726446fe20981642ea4747041bd59241749bdf
Aimless397 <douglaszs397@hotmail.com>
2022-04-01 18:21:00 -0500

    fix: formatted module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { SendgridService } from '../email/services/sendgrid.service';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './controllers/auth.controller';
+import { AuthService } from './services/auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
-import { AuthService } from './services/auth.service';
-import { SendgridService } from '../email/services/sendgrid.service';
 
 @Module({
   imports: [
